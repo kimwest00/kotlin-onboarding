1e4fbf93e2aec8a1500d5fdd95f5652e315504e8
kimwest00 <minseohufs@hufs.ac.kr>
2023-07-30 16:58:31 +0900

    chore : TODO 작성

--- src/main/kotlin/onboarding/Problem1.kt
+++ src/main/kotlin/onboarding/Problem1.kt
@@ -1,5 +1,19 @@
 package onboarding
 
 fun solution1(pobi: List<Int>, crong: List<Int>): Int {
-    TODO("프로그램 구현")
+    //calcuateMax ; pobi, crong 각각에서 반복되므로 함수로 분리한다;
+
+    TODO("4. calcuateMax 과정을 list 길이(=2)만큼 반복한다")
+    TODO("5. pobi, crong 각각의 max값을 반환한다")
+    TODO("6. 4의 결과를 비교하고 return한다")
+    //1 :  포비 승 , 2 크롱 승, 0 : 무승부, -1 : 예외
 }
+
+//fdsfs
+fun calcuateMax(page:Int):Int{
+    TODO("1. 입력값(int)의 길이만큼 자릿값들 리스트 형태로 저장한다")
+    TODO("2. 1에서 구한 리스트에서 loop를 돌며")
+    TODO("3. +, x 연산을 한다")
+}
+
+
